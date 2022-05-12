7edcfb09c8c2ff996dd8a0438da2583ad31834fe
Abdelrhman-Salah-Ghanem <ghanem.ana.abdo@gmail.com>
2022-05-12 19:21:01 +0200

    Auth Api

--- app/src/main/java/com/example/e_exam/network/LevelsAndDepartment/Department.kt
+++
@@ -1,6 +0,0 @@
-package com.example.e_exam.network.LevelsAndDepartment
-
-data class Department (
-    val id : Int,
-    val name : String,
-)
--- app/src/main/java/com/example/e_exam/network/LevelsAndDepartment/Level.kt
+++
@@ -1,6 +0,0 @@
-package com.example.e_exam.network.LevelsAndDepartment
-
-data class Level (
-    val id : Int,
-    val name : String,
-)
